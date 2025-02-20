4db9a9131c2ce74782ea74ac556cd5dbf93101c8
Mateus Perdigão <mateusperdigao@hotmail.com>
2025-02-20 15:47:50 -0300

    add app with lint issues

---
+++ sample-app/sample.js
@@ -0,0 +1,18 @@
+var name = "John"  
+
+function greet(){
+    console.log("Hello, " + name)
+    return
+    {
+        message: "Welcome!"
+    }
+}
+
+greet()
+
+function add(a, b) {
+    return a + b;
+}
+
+const result = add(  5,3 )
+console.log(result)
