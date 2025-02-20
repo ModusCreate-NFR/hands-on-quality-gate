2fbac6be07ebd0725934ab50af4bab086a62995d
Mateus Perdigão <mateusperdigao@hotmail.com>
2025-02-20 18:44:43 -0300

    Create sample2.js

---
+++ sample-app/sample2.js
@@ -0,0 +1,21 @@
+var name = "John"  
+
+function greet(){
+    console.log("Hello, " + name)
+    return
+    {
+        message: "Welcome!"
+    }
+}
+
+greet()
+
+function add(a, b) {
+    return a + b;
+}
+
+const result = add(  5,3 )
+console.log(result)
+
+if(true) 
+console.log("This is an unbraced if statement")
